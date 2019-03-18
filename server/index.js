220c292776bf76d187aac417ab4c45e786fad5ff
Karan Kotwal <kotwalkaran64@gmail.com>
2019-03-18 02:27:06 -0400

    Updating server

--- server/index.js
+++ server/index.js
@@ -16,5 +16,5 @@ app.use(morgan("dev"));
 apollo.applyMiddleware({ app, path: "/graphql" });
 
 app.listen({ port: 8000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:8000/graphql$`)
+  console.log(`🚀 Server ready at http://localhost:8000/graphql`)
 );
